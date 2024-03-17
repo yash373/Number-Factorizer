5ff0ad6698e368ee2ccfdc99dd2b28c2638d3d16
Yasodhan <yash.yasodhan.123@gmail.com>
2024-03-17 10:07:01 +0530

    finished algortihm

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def factorize(n):
-    pass
+    factors = []
+    i = 2
+
+    while n > 1:
+        if n % i == 0:
+            factors.append(i)
+            n //= i
+        else:
+            i += 1
+    
+    return factors
 
 if __name__ == '__main__':
     print(factorize(int(input("Enter Number to Factorize: "))))
